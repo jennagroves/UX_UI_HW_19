987b72f3a3b44e88049fcbaf6437c643d43184f3
jennagroves <jennagroves99@gmail.com>
2023-10-18 17:48:06 -0500

    Added more jQuery

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
-$("button").on("click", function(){
-    $()
-})
+console.log("hello!!!");
+$(".card").hover (function(){
+    $(".card").animate({width: "275px"});
+    $(".container").css("color", "#60933B");
+});
